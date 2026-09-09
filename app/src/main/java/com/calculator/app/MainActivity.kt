43851339b164813a17e3138f2e71328de22bc031
kusa21gi-star <kusa21gi@gmail.com>
2026-09-09 16:55:44 +0530

    Add app/src/main/java/com/calculator/app/MainActivity.kt

---
+++ app/src/main/java/com/calculator/app/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.calculator.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.settings.allowFileAccessFromFileURLs = true
+        webView.settings.allowUniversalAccessFromFileURLs = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("file:///android_asset/www/index.html")
+        setContentView(webView)
+    }
+    override fun onBackPressed() {
+        val webView = findViewById<WebView>(android.R.id.content).getChildAt(0) as? WebView
+        if (webView?.canGoBack() == true) webView.goBack() else super.onBackPressed()
+    }
+}
